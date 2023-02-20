429f8f4dc31255babe48d6bc72a283018d01f746
Seungjin Kang <xeungjin@gmail.com>
2023-02-20 12:53:59 +0900

    ~97page

--- src/test/java/com/shop/ItemRepositoryTest.java
+++ src/test/java/com/shop/ItemRepositoryTest.java
@@ -77,7 +77,12 @@ class ItemRepositoryTest {
     }
 
     @Test
-    void contextLoads() {
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest() {
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for(Item item:itemList) {
+            System.out.println(item.toString());
+        }
     }
-
 }
--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package com.shop.repository;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ItemRepositoryTest {
-
-}
